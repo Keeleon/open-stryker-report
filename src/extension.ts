1e11c50a9e27369cb8b3818dc1795b1d8209fa49
Craig Simko <craigs1701@gmail.com>
2018-04-06 22:20:07 -0400

    HTML file that matches open script now opens in browser.
    If the current file that is open in the editor matches a file found in "reports/mutation/html/**/" then that file is opened in the user's default browser. If no match is found then the default entry point for reports (i.e. "reports/mutation/html/index.html") is opened instead.

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,54 @@
 'use strict';
 import * as vscode from 'vscode';
+import { StatusBarAlignment } from 'vscode';
+import open = require('open');
+import * as path from 'path';
+
+const COMMAND_OPEN_STRYKER_REPORT: string = 'openStrykerReport';
+const STATUS_BAR_ALIGNMENT: StatusBarAlignment = StatusBarAlignment.Left;
+const STATUS_BAR_PRIORITY: number = 1;
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('"open-stryker-report" detected a stryker.conf.js file and has activated');
-  const disposable = vscode.window.onDidChangeActiveTextEditor(displayOpenStrykerReportButton);
-  context.subscriptions.push(disposable);
+  displayStrykerStatusBarItem();
+  const command = vscode.commands.registerCommand(COMMAND_OPEN_STRYKER_REPORT, openStrykerReport);
+  context.subscriptions.push(command);
 }
 
-function displayOpenStrykerReportButton(textEditor: vscode.TextEditor | undefined) {
-  if (typeof textEditor === 'undefined') {
-    return;
+function displayStrykerStatusBarItem(): void {
+  const openStrykerReportItem: vscode.StatusBarItem =
+    vscode.window.createStatusBarItem(STATUS_BAR_ALIGNMENT, STATUS_BAR_PRIORITY);
+  openStrykerReportItem.tooltip = 'Open Stryker HTML Report';
+  openStrykerReportItem.command = COMMAND_OPEN_STRYKER_REPORT;
+  openStrykerReportItem.text = 'Stryker';
+  openStrykerReportItem.show();
+}
+
+async function openStrykerReport() {
+  const strykerReportLocation = await getStrykerReportLocation();
+  open(strykerReportLocation);
+}
+
+async function getStrykerReportLocation(): Promise<string> {
+ const currentOpenFileName = getCurrentOpenFileName();
+ const defaultStrykerReportLocation = `${vscode.workspace.rootPath}\\reports\\mutation\\html\\index.html`;
+ if (currentOpenFileName === '') {
+   return defaultStrykerReportLocation;
+ }
+ const extension = path.extname(currentOpenFileName);
+ const baseName = path.basename(currentOpenFileName, extension);
+ const values = await vscode.workspace.findFiles(`reports/mutation/html/**/${baseName}${extension}.html`);
+ if (values.length === 0) {
+   return defaultStrykerReportLocation;
+ }
+ return values[0].fsPath;
+}
+
+function getCurrentOpenFileName(): string {
+  if (typeof vscode.window.activeTextEditor === 'undefined') {
+    return '';
   }
-  console.log(textEditor.document.fileName);
+  return vscode.window.activeTextEditor.document.fileName;
 }
 
 export function deactivate() {
